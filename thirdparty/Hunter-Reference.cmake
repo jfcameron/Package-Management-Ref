15fd38ad6f29e56d7bb9d3f4387ec645e82468c1
Joseph Cameron <jf.cameron@gmail.com>
2018-04-04 17:21:06 -0400

    wip

--- thirdparty/Hunter-Reference.cmake
+++ thirdparty/Hunter-Reference.cmake
@@ -9,8 +9,8 @@ if((NOT HUNTER-REFERENCE_INCLUDE_DIR) OR (NOT EXISTS ${HUNTER-REFERENCE_INCLUDE_
     message(STATUS "Initializing dependency \"Hunter-Reference\"")
 
     execute_process(
-        COMMAND git submodule update --init -- Hunter-Reference
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND git submodule update --init -- thirdparty/Hunter-Reference
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
 
     set(HUNTER-REFERENCE_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/include
@@ -25,55 +25,3 @@ if((NOT HUNTER-REFERENCE_INCLUDE_DIR) OR (NOT EXISTS ${HUNTER-REFERENCE_INCLUDE_
 else()
     message(STATUS "dependency \"Hunter-Reference\" already initialized. Skipping")
 endif()
-
-#[[### WORK ON HEADER ONLY ###
-find_path(HUNTER-REFERENCE_INCLUDE_DIR ...)
-
-if((NOT HUNTER-REFERENCE_INCLUDE_DIR) OR (NOT EXISTS ${HUNTER-REFERENCE_INCLUDE_DIR}))
-    message(STATUS "Initializing dependency \"Hunter-Reference\"")
-
-    execute_process(
-        COMMAND git submodule update --init -- Hunter-Reference
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-
-    set(HUNTER-REFERENCE_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/include
-        CACHE PATH "hunter-reference include directory" FORCE
-    )
-else()
-    message(STATUS "dependency \"Hunter-Reference\" already initialized. Skipping")
-endif()
-]]
-
-##### REFERENCE #######
-#[[# step 0
-find_path(FOO_INCLUDE_DIR ...)
-
-if((NOT FOO_INCLUDE_DIR) OR (NOT EXISTS ${FOO_INCLUDE_DIR})
-    # we couldn't find the header files for FOO or they don't exist
-    message("Unable to find foo")
-
-    # we have a submodule setup for foo, assume it is under external/foo
-    # now we need to clone this submodule
-    execute_process(COMMAND git submodule update --init -- external/foo
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-
-    # set FOO_INCLUDE_DIR properly
-    set(FOO_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external/foo/path/to/include
-        CACHE PATH "foo include directory")
-
-    # also install it
-    install(DIRECTORY ${FOO_INCLUDE_DIR}/foo DESTINATION ${some_dest})
-
-    # for convenience setup a target
-    add_library(foo INTERFACE)
-    target_include_directories(foo INTERFACE
-                               $<BUILD_INTERFACE:${FOO_INCLUDE_DIR}>
-                               $<INSTALL_INTERFACE:${some_dest}>)
-
-    # need to export target as well
-    install(TARGETS foo EXPORT my_export_set DESTINATION ${some_dest})
-else()
-    # see above, setup target as well
-endif()
-]]
