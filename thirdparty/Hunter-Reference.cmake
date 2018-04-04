16c573b99ff198a0937e73a82303e6f341ce0a82
Joseph Cameron <jf.cameron@gmail.com>
2018-04-04 14:28:23 -0400

    wip

--- thirdparty/Hunter-Reference.cmake
+++ thirdparty/Hunter-Reference.cmake
@@ -13,14 +13,39 @@ if((NOT HUNTER-REFERENCE_INCLUDE_DIR) OR (NOT EXISTS ${HUNTER-REFERENCE_INCLUDE_
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )
 
-    set(HUNTER-REFERENCE_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/include"
+    set(HUNTER-REFERENCE_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/include
         CACHE PATH "hunter-reference include directory" FORCE
     )
 
+    set(HUNTER-REFERENCE_LIBRARIES ${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/build/libHunter-Reference.a;glfw
+        CACHE PATH "hunter-reference output libraries" FORCE
+    )
+
+    add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference")
 else()
     message(STATUS "dependency \"Hunter-Reference\" already initialized. Skipping")
 endif()
 
+#[[### WORK ON HEADER ONLY ###
+find_path(HUNTER-REFERENCE_INCLUDE_DIR ...)
+
+if((NOT HUNTER-REFERENCE_INCLUDE_DIR) OR (NOT EXISTS ${HUNTER-REFERENCE_INCLUDE_DIR}))
+    message(STATUS "Initializing dependency \"Hunter-Reference\"")
+
+    execute_process(
+        COMMAND git submodule update --init -- Hunter-Reference
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+
+    set(HUNTER-REFERENCE_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Hunter-Reference/include
+        CACHE PATH "hunter-reference include directory" FORCE
+    )
+else()
+    message(STATUS "dependency \"Hunter-Reference\" already initialized. Skipping")
+endif()
+]]
+
+##### REFERENCE #######
 #[[# step 0
 find_path(FOO_INCLUDE_DIR ...)
 
