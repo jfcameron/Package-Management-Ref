3fe2fc499ce20268e497d6cf0982617c7fee0594
Joseph Cameron <jf.cameron@gmail.com>
2018-04-05 22:30:01 -0400

    wip

--- docs/generate-readme.sh
+++ docs/generate-readme.sh
@@ -53,7 +53,7 @@ GenerateReadme()
         local output=""
 
         for ImageURL in $*; do 
-            output+="<img src=\"http://jfcameron.github.io/Github/${PROJECT_NAME}/images/${ImageURL}\" width=\"100%\">"
+            output+="<img src=\"http://jfcameron.github.io/${PROJECT_NAME}/images/${ImageURL}\" width=\"100%\">"
         done
 
         echo "${output}"
